215ca92b04e311c1eb3edbd5a6ea4571e66a4dcf
indigoyeoma <nfsshift2@naver.com>
2022-11-05 14:51:48 +0900

    changed path

--- examples/model_loading_example.py
+++ examples/model_loading_example.py
@@ -7,7 +7,8 @@ import numpy as np
 import random
 
 
-MODEL_PATH = "../src/env/mujoco/assets/dynamic_grasping.xml"
+# MODEL_PATH = "../src/env/mujoco/assets/dynamic_grasping.xml"
+MODEL_PATH ='/home/yeoma/code/mm-wbc/src/env/mujoco/assets/dynamic_grasping.xml'
 # MODEL_PATH = "../../.mujoco/mujoco-py/xmls/tosser.xml"
 
 model = mujoco_py.load_model_from_path(MODEL_PATH)
@@ -16,16 +17,16 @@ sim = mujoco_py.MjSim(model)
 viewr = mujoco_py.MjViewer(sim=sim)
 
 state = sim.get_state()
-print("state 1: ", state)
-qpos = np.array(list(map(lambda x: x*random.random(),np.ones(shape=(25,)))))
+# print("state 1: ", state)
+# qpos = np.array(list(map(lambda x: x*random.random(),np.ones(shape=(25,)))))
 
-qvel = np.array(list(map(lambda x: x*random.random(),np.ones(shape=(25,)))))
+# qvel = np.array(list(map(lambda x: x*random.random(),np.ones(shape=(25,)))))
 
-state = mujoco_py.MjSimState(state.time, qpos, qvel, state.act, state.udd_state)
+# state = mujoco_py.MjSimState(state.time, qpos, qvel, state.act, state.udd_state)
 
-print("modified state : ", state)
+# print("modified state : ", state)
 
-print(sim.mocap_pos)
+# print(sim.mocap_pos)
 
 
 # sim_state = sim.get_state()
